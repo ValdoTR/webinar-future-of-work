bb4ec836fbef7d104dda65ae8ae7776684d69f39
ValdoTR <vteixeiraromao@gmail.com>
2022-03-28 16:32:12 +0000

    Deploying to gh-pages from master @ 637b5ca88c9309788819bcae60345eb53616f2ce 🚀

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,16 @@
 /// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
+import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
-import {bootstrapExtra} from "@workadventure/scripting-api-extra";
+console.log('Script started successfully');
 
-// The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure.
-bootstrapExtra().catch(e => console.error(e));
+// Waiting for the API to be ready
+WA.onInit().then(() => {
+    console.log('Scripting API ready');
+    console.log('Player tags: ',WA.player.tags)
 
-let currentPopup: any = undefined;
-const today = new Date();
-const time = today.getHours() + ":" + today.getMinutes();
-
-WA.room.onEnterZone('clock', () => {
-    currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
-})
-
-WA.room.onLeaveZone('clock', closePopUp)
-
-function closePopUp(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
-}
+    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
+    bootstrapExtra().then(() => {
+        console.log('Scripting API Extra ready');
+    }).catch(e => console.error(e));
+    
+}).catch(e => console.error(e));
